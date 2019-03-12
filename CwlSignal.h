92ed9174f3ebe6fb7d69e8c7f6ffdab145487b01
Charlie Osmer <charlie.osmer@gmail.com>
2019-03-12 07:33:40 -0400

    Initial commit

---
+++ CwlSignal.h
@@ -0,0 +1,19 @@
+//
+//  CwlSignal.h
+//  CwlSignal
+//
+//  Created by Charles Osmer on 3/12/19.
+//  Copyright © 2019 Charlie Osmer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CwlSignal.
+FOUNDATION_EXPORT double CwlSignalVersionNumber;
+
+//! Project version string for CwlSignal.
+FOUNDATION_EXPORT const unsigned char CwlSignalVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CwlSignal/PublicHeader.h>
+
+
